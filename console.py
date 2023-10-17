8685f8a6d618d700337e9cd958c80d993b90aeb8
peter <01peterkinyanjui@gmail.com>
2023-10-17 16:52:44 -0700

    task9

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ from models.base_model import BaseModel
 import shlex
 import datetime
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 storage = FileStorage()
 storage.reload()
