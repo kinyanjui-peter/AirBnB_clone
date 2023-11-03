9d2cfb931503ebb4970e1957405219941739f3b6
peter <01peterkinyanjui@gmail.com>
2023-11-02 20:12:05 -0700

    AirBnB-clone-console

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,44 @@
 #!/usr/bin/python3
-"""This script is the base model"""
+"""Defines the Base Model"""
 
-import uuid
-from datetime import datetime
-from models import storage
+
+from uuid import uuid4
+from datetime import date, datetime
+import models
 
 
 class BaseModel:
-
-    """Class from which all other classes will inherit"""
-
+    """The super class for other classes"""
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
-
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        """Initializes the base model with optional keyword arguments"""
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                elif k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.fromisoformat(v))
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        """Returns a string representation of the base model"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-
+        """Updates the 'updated_at' attribute with the current datetime and saves to storage"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """Returns a dictionary representation of the base model"""
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
